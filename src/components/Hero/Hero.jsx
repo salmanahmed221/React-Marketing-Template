54ebf9d560bca6ea4c41ab1360d7013571d2e7ff
salmanahmed221 <salmanahmed121221@gmail.com>
2024-08-30 04:41:39 -0700

    fix another deployment error

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { SliderData } from "./sliderData/SliderData";
+import SliderData from "./sliderData/sliderData";
 
 function Hero() {
   return (
